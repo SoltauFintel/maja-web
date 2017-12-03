22b74913673db839085d93c06492c1a35ec22ced
Marcus Warm <warm@mwvb.de>
2017-12-03 17:25:24 +0100

    removed print

--- maja-web/src/main/java/de/mwvb/maja/web/Broadcaster.java
+++ maja-web/src/main/java/de/mwvb/maja/web/Broadcaster.java
@@ -14,7 +14,6 @@ public class Broadcaster {
 	}
 	
 	public void broadcast(String topic, String data) {
-		System.out.println(listeners.size() + " listeners: " + topic + " = " + data);
 		listeners.forEach(listener -> listener.handle(topic, data));
 	}
 }
