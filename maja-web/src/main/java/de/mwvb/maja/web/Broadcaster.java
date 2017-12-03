6d6373fdb6701a65bfd5178b35281f1009862eae
Marcus Warm <warm@mwvb.de>
2017-12-03 16:20:08 +0100

    fix

--- maja-web/src/main/java/de/mwvb/maja/web/Broadcaster.java
+++ maja-web/src/main/java/de/mwvb/maja/web/Broadcaster.java
@@ -3,6 +3,9 @@ package de.mwvb.maja.web;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.inject.Singleton;
+
+@Singleton
 public class Broadcaster {
 	private final List<BroadcastListener> listeners = new ArrayList<>();
 	
@@ -11,6 +14,7 @@ public class Broadcaster {
 	}
 	
 	public void broadcast(String topic, String data) {
+		System.out.println(listeners.size() + " listeners: " + topic + " = " + data);
 		listeners.forEach(listener -> listener.handle(topic, data));
 	}
 }
